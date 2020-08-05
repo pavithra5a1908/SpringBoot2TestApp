fc192df7c1e6561a62f129b31032e6f115073896
Pavithra <Pavithra@DESKTOP-7MIK8I9>
2020-08-05 05:05:08 -0700

    WH-5820-First Time File Update

---
+++ src/main/java/in/nareshit/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package in.nareshit.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class TestRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+     System.out.println("From runner");
+	}
+
+}
